39d34b4c70ec5d60f65b105a0314a9f35d52e1ff
Ali AlSadah <aalsadah@ucsd.edu>
2026-08-02 12:50:44 -0700

    form native html, and css validation implemented. Writing JS script for error reporting and error message announcing

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const form=document.getElementById("contact-form");
+const email=form.elements["email"];
+const name=form.elements["name"];
+const message=form.elements["message"]
+const emailOut=form.elements["email-output"];
+const nameOut=form.elements["name-output"];
+const messageOut=form.elements["message-output"];
+
+function updateOuts(){
+
+}
+
+function validateFields(){
+    const vName=name.validity;
+}
+
+function getErrorTypes(){
+
+}
+
+function announceErrors(){
+
+}
+
+function collectErrorsJson(){
+
+}
